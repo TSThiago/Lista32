57f731b4810d300349316b5557abf6246c9af46d
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-10-06 23:04:07 -0300

    Término do exercício 3.

--- exs.js
+++ exs.js
@@ -17,28 +17,56 @@
 //     return stringNovo
 // }
 // Ex 2:
-console.log(ValidarNomeDeUsuario("_SeniorDev2"));
-function ValidarNomeDeUsuario(nome) {
-    var arrayNumeros = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-    var re = /\w/g;
-    var validacao = true;
-    if (nome != nome.match(re).join('')) {
-        console.log("Presença de caracteres inválidos.");
-        validacao = false;
-    }
-    if (nome.length < 4 || nome.length > 25) {
-        validacao = false;
-    }
-    arrayNumeros.forEach(function (x) {
-        if (x == nome.charAt(0)) {
-            validacao = false;
-            return;
-        }
+// console.log(ValidarNomeDeUsuario("_SeniorDev2"))
+// function ValidarNomeDeUsuario(nome : string) : boolean{
+//     let arrayNumeros : string[] = ["1","2","3","4","5","6","7","8","9","0"] 
+//     let re : RegExp = /\w/g 
+//     let validacao : boolean = true
+//     if(nome != nome.match(re).join('')){
+//         console.log("Presença de caracteres inválidos.")
+//         validacao = false
+//     }
+//     if(nome.length < 4 || nome.length > 25){
+//         validacao = false
+//     }
+//     arrayNumeros.forEach(x => {
+//         if(x == nome.charAt(0)){
+//             validacao = false
+//             return
+//         }
+//     })
+//     if(nome.charAt(0) == "_")
+//     validacao = false
+//     if(nome.charAt(nome.length - 1) == "_"){
+//         validacao = false
+//     }
+//     return validacao
+// }
+// Ex 3:
+var array = ["2, 4, 5, 8, 14", "2, 3, 5, 14, 16"];
+console.log(MostrarElementosEmComum(array));
+function MostrarElementosEmComum(arrayString) {
+    var primeiroArray = [];
+    var segundoArray = [];
+    var arrayFinal = [];
+    var mensagem = false;
+    primeiroArray = arrayString[0].split(",");
+    console.log(primeiroArray);
+    segundoArray = arrayString[1].split(",");
+    console.log(segundoArray);
+    primeiroArray.forEach(function (x) {
+        segundoArray.forEach(function (y) {
+            if (x == y) {
+                console.log(arrayFinal);
+                arrayFinal.push(x);
+                mensagem = true;
+            }
+        });
     });
-    if (nome.charAt(0) == "_")
-        validacao = false;
-    if (nome.charAt(nome.length - 1) == "_") {
-        validacao = false;
+    if (mensagem == false) {
+        return "false";
     }
-    return validacao;
+    else {
+        return arrayFinal.join(", ");
+    }
 }
--- exs.ts
+++ exs.ts
@@ -22,31 +22,64 @@
 
 // Ex 2:
 
-console.log(ValidarNomeDeUsuario("_SeniorDev2"))
+// console.log(ValidarNomeDeUsuario("_SeniorDev2"))
 
-function ValidarNomeDeUsuario(nome : string) : boolean{
-    let arrayNumeros : string[] = ["1","2","3","4","5","6","7","8","9","0"] 
-    let re : RegExp = /\w/g 
-    let validacao : boolean = true
-    if(nome != nome.match(re).join('')){
-        console.log("Presença de caracteres inválidos.")
-        validacao = false
-    }
+// function ValidarNomeDeUsuario(nome : string) : boolean{
+//     let arrayNumeros : string[] = ["1","2","3","4","5","6","7","8","9","0"] 
+//     let re : RegExp = /\w/g 
+//     let validacao : boolean = true
+//     if(nome != nome.match(re).join('')){
+//         console.log("Presença de caracteres inválidos.")
+//         validacao = false
+//     }
 
-    if(nome.length < 4 || nome.length > 25){
-        validacao = false
-    }
-    arrayNumeros.forEach(x => {
-        if(x == nome.charAt(0)){
-            validacao = false
-            return
-        }
+//     if(nome.length < 4 || nome.length > 25){
+//         validacao = false
+//     }
+//     arrayNumeros.forEach(x => {
+//         if(x == nome.charAt(0)){
+//             validacao = false
+//             return
+//         }
+//     })
+//     if(nome.charAt(0) == "_")
+//     validacao = false
+//     if(nome.charAt(nome.length - 1) == "_"){
+//         validacao = false
+//     }
+//     return validacao
+// }
+
+// Ex 3:
+
+let array : string[] = ["2, 4, 5, 8, 14", "2, 3, 5, 14, 16"]
+
+console.log(MostrarElementosEmComum(array))
+
+function MostrarElementosEmComum(arrayString : string[]) : string{
+    let primeiroArray : string[] = []
+    let segundoArray : string[] = []
+    let arrayFinal : string[] = []
+    let mensagem : boolean = false
+
+    primeiroArray = arrayString[0].split(",") 
+    console.log(primeiroArray)
+
+    segundoArray = arrayString[1].split(",")
+    console.log(segundoArray)
+
+    primeiroArray.forEach(x => {
+        segundoArray.forEach(y => {
+            if(x == y){
+                console.log(arrayFinal)
+                arrayFinal.push(x)
+                mensagem = true
+            }
+        })
     })
-    if(nome.charAt(0) == "_")
-    validacao = false
-    if(nome.charAt(nome.length - 1) == "_"){
-        validacao = false
+    if(mensagem == false){
+        return "false"
+    }else{
+        return arrayFinal.join(", ")
     }
-    return validacao
 }
-
